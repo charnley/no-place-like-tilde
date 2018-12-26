5a6c5df368b94a9d1c90f2f3aacf4d6b55a4041f
Jimmy Charnley Kromann <jimmy@charnley.dk>
2018-12-26 16:29:34 +0100

    apartmentpipeline

---
+++ pipelines.py
@@ -0,0 +1,58 @@
+
+from sqlalchemy.orm import sessionmaker
+
+import json
+import models
+
+class ApartmentPipeline(object):
+
+    def __init__(self):
+
+        engine = models.db_connect()
+        models.create_table(engine)
+        self.Session = sessionmaker(bind=engine)
+
+
+    def process_item(self, item, spider):
+        """
+
+        This method is called for every returned item
+
+        """
+
+        session = self.Session()
+
+        # convert from ApartmentItem to ApartmentModel
+        model = models.ApartmentModel()
+        model.address = item['address']
+        model.rent = item['rent']
+        model.rooms = item['rooms']
+
+        try:
+            session.add(model)
+            session.commit()
+
+        except:
+            session.rollback()
+            raise
+
+        finally:
+            session.close()
+
+        return item
+
+
+
+class ApartmentJsonPipeline(object):
+
+    def open_spider(self, spider):
+        self.file = open('item.jl', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
+        return item
+
