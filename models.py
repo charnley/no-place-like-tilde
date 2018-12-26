b86beb47e5c981bd544606a1ea4272c0be78c809
Jimmy Charnley Kromann <jimmy@charnley.dk>
2018-12-26 16:07:23 +0100

    apartment model

--- models.py
+++ models.py
@@ -20,7 +20,10 @@ def db_connect():
     Performs database connection using database settings from settings.py.
     Returns sqlalchemy engine instance
     """
-    return create_engine(get_project_settings().get("CONNECTION_STRING"))
+
+    connect_string = "sqlite:///database.db"
+
+    return create_engine(connect_string)
 
 def create_table(engine):
     DeclarativeBase.metadata.create_all(engine)
@@ -29,5 +32,27 @@ class ApartmentModel(DeclarativeBase):
     __tablename__ = "apartment_table"
 
     id = Column(Integer, primary_key=True)
-    quote = Column('quote', Text())
-    author = Column('author', String(100))
+
+    index = Column('index', Integer)
+    address = Column('address', String())
+    rent = Column('rent', Float())
+    livingspace = Column('squaremeter', Float()) # in m**2
+
+    # images = scrapy.Field()
+    # appendix = scrapy.Field()
+
+    # title = scrapy.Field()
+    # description = scrapy.Field()
+
+    feature_type = Column('apartment_type', String())
+    feature_rooms = Column('rooms', String())
+    feature_floor = Column('floor', String())
+    feature_available = Column('avaliable', String())
+
+    def __repr__(self):
+
+        return "<Apartment {:} m**2 / {:} chf - {:}>".format(
+            self.livingspace,
+            self.rent,
+            self.address)
+
