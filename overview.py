643620d6ccea947beb6f7c3ecc537b5e335e4401
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-01-18 12:45:45 +0100

    updated overview

--- overview.py
+++ overview.py
@@ -21,6 +21,7 @@ def main():
 
     distances = []
     ratios = []
+    sqm = []
 
     for apartment in apartments:
 
@@ -32,19 +33,41 @@ def main():
         rent = apartment.rent
         livingspace = apartment.livingspace
         if livingspace is None: continue
+        if livingspace < 10: continue
         if rent is None: continue
         ratio = rent / livingspace
 
+        if ratio > 75: continue
+
         distances.append(distance)
         ratios.append(ratio)
+        sqm.append(livingspace)
 
 
-    print(distances)
-    print(ratios)
+    # print(distances)
+    # print(ratios)
 
-    plt.plot(distances, ratios, 'k.')
-    plt.ylim([0, 30])
-    plt.savefig("fig_ratio_overview")
+
+    fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+
+    ax.plot(sqm, ratios, 'k.')
+
+    ax.set_xlabel('Livingspace [m2]', fontweight='medium', fontsize=13)
+    ax.set_ylabel('Ratio [chf/m2]', fontweight='medium', fontsize=13)
+
+
+    handles, labels = ax.get_legend_handles_labels()
+    handles = [h[0] for h in handles]
+    # ax.legend(handles, labels, loc='upper right', frameon=False, numpoints=1)
+
+    ax.yaxis.grid(True, zorder=0)
+    ax.xaxis.grid(True, zorder=0)
+
+    filename = "fig_ratio_overview"
+
+    plt.savefig(filename + ".png", bbox_inches="tight")
+    plt.savefig(filename + ".pdf", bbox_inches="tight")
+
 
 
     return
